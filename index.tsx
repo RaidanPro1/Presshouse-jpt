4f03c1d1e8fe87bca6263083ef4501e88db48ea6
Raidan Ameen <info@raidan.pro>
2026-01-30 05:37:19 +0300

    feat: Enable zoneless change detection
    Implements zoneless change detection and sets `ngZone` to 'noop' for improved performance and efficiency in the application bootstrapping process.

--- index.tsx
+++ index.tsx
@@ -1,16 +1,14 @@
-
 import '@angular/compiler';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { provideZonelessChangeDetection } from '@angular/core';
 
 import { AppComponent } from './src/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideZonelessChangeDetection(),
     provideHttpClient(),
   ],
+  ngZone: 'noop'
 }).catch((err) => console.error(err));
 
 // AI Studio always uses an `index.tsx` file for all project types.
